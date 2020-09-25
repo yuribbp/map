241f8ff1e581b694e11b53eebadc2dc6595bccd1
Yuri Baldan <yuribbp@gmail.com>
2020-09-25 16:53:42 -0300

    Initial implementation

---
+++ src/application/Program.java
@@ -0,0 +1,21 @@
+package application;
+
+import java.util.Map;
+import java.util.TreeMap;
+
+public class Program {
+
+	public static void main(String[] args) {
+
+		Map<String, String> cookies = new TreeMap<>();
+
+		cookies.put("username", "Maria");
+		cookies.put("email", "maria@gmail.com");
+		cookies.put("phone", "99711122");
+
+		System.out.println("ALL COOKIES:");
+		for (String key : cookies.keySet()) {
+			System.out.println(key + ": " + cookies.get(key));
+		}
+	}
+}
